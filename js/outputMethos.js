849bd3a57405c4e74d8da0893d9c30209b6eec54
Vinaym0423 <vinay.m@spurtreetech.in>
2024-03-17 19:11:37 +0530

    gradients, box model,nav bar assignement, position properties and javascript datatypes,objects,functions and array

---
+++ js/outputMethos.js
@@ -0,0 +1,23 @@
+console.log('Hiii')
+console.log('these is console statement')
+
+window.alert('Is these is a simple alert with One Option')
+
+var ans= window.confirm('Is these is a confirmation alert with two options')
+
+if(ans==true){
+    alert('You have clicked on Ok')
+} else{
+    alert('You have not clicked on Ok')
+}
+
+var age= window.prompt('Enter your age and proceed')
+
+if(age<=18) alert('You are below 18')
+else alert('You are above 18')
+
+document.write('It was written on webpage')
+document.writeln('it is webpage')
+document.write('It will take space before starting because of writeln')
+
+console.log(1+2)
---
+++ js/variable.js
@@ -0,0 +1,41 @@
+// console.log(num); //hoisting
+// var num=10;
+// console.log('Declaration and initialization '+num)
+// num=100;
+// console.log('re initialization '+num);
+// var num='hello'
+// console.log('re decalration '+num);
+
+
+// console.log(sal);
+// let sal=100000;
+// console.log('declaration and initialization '+ sal);
+
+// sal=200000
+// console.log('re initialization '+sal);
+
+// let sal='helll0'
+// console.log('re decalration '+sal);
+
+// console.log(fname); //hoisting
+//  fname='abc';
+// console.log('Declaration and initialization '+fname)
+// fname='xyz';
+// console.log('re initialization '+fname);
+// const fname=123
+// console.log('re decalration '+fname);
+
+// console.log(num);
+// let num=10;
+// let num1=20;
+// console.log(num+num1);
+// num1=50;
+// console.log(num1);
+
+console.log(b);
+let a=10;
+var b=56;
+console.log(a+b);
+b=50;
+console.log(b);
+
